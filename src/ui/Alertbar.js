NgoKimPhu/touchstonejs

commit 5d82228612b3b170f63456172377df66b70e2298

Upgrade to react 0.14 #fixes 89
There are still warns for ReactCSSTransitionGroup timeout property not
being set, but it makes the current animations stop working

src/ui/Alertbar.js
@@ -1,6 +1,6 @@
-var React = require('react/addons');
+var React = require('react');
 var classnames = require('classnames');
-var Transition = React.addons.CSSTransitionGroup;
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 module.exports = React.createClass({
 	displayName: 'Alertbar',
@@ -29,9 +29,9 @@ module.exports = React.createClass({
 		var animatedBar = this.props.visible ? <div className={className}>{pulseWrap}</div> : null;
 
 		var component = this.props.animated ? (
-			<Transition transitionName="Alertbar" component="div">
+			<ReactCSSTransitionGroup transitionName="Alertbar" transitionEnterTimeout={300} transitionLeaveTimeout={300} component="div">
 				{animatedBar}
-			</Transition>
+			</ReactCSSTransitionGroup>
 		) : <div className={className}>{pulseWrap}</div>;
 
 		return component;

src/core/ViewManager.js
@@ -1,8 +1,8 @@
 var blacklist = require('blacklist');
 var classNames = require('classnames');
 var ErrorView = require('./ErrorView');
-var React = require('react/addons');
-var Transition = React.addons.CSSTransitionGroup;
+var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 function createViewsFromChildren (children) {
 	var views = {};
@@ -142,9 +142,9 @@ var ViewManager = React.createClass({
 			transitionName = 'view-transition-' + this.state.options.transition;
 		}
 		return (
-			<Transition transitionName={transitionName} transitionEnter={true} transitionLeave={true} className={className} component="div">
+			<ReactCSSTransitionGroup transitionName={transitionName} transitionEnter={true} transitionLeave={true} className={className} component="div">
 				{viewContainer}
-			</Transition>
+			</ReactCSSTransitionGroup>
 		);
 	}
 });

src/core/animation.js
@@ -1,5 +1,5 @@
 var animation = require('tween.js');
-var React = require('react');
+var ReactDOM = require('react-dom');
 
 function update () {
 	animation.update();
@@ -50,7 +50,7 @@ Mixins.ScrollContainerToTop = {
 	},
 	scrollContainerToTop () {
 		if (!this.isMounted() || !this.refs.scrollContainer) return;
-		this._scrollContainerAnimation = scrollToTop(React.findDOMNode(this.refs.scrollContainer), {
+		this._scrollContainerAnimation = scrollToTop(ReactDOM.findNode(this.refs.scrollContainer), {
 			onComplete: () => { delete this._scrollContainerAnimation; }
 		});
 	}

src/ui/Button.js
@@ -1,4 +1,4 @@
-var React = require('react/addons');
+var React = require('react');
 var Tappable = require('react-tappable');
 
 var blacklist = require('blacklist');

src/ui/ButtonGroup.js
@@ -1,6 +1,6 @@
 var blacklist = require('blacklist');
 var classnames = require('classnames');
-var React = require('react/addons');
+var React = require('react');
 
 module.exports = React.createClass({
 	displayName: 'ButtonGroup',

src/ui/DatePicker.js
@@ -1,4 +1,4 @@
-var React = require('react/addons');
+var React = require('react');
 var Tappable = require('react-tappable');
 var classnames = require('classnames');
 

src/ui/DatePickerPopup.js
@@ -1,5 +1,5 @@
 var blacklist = require('blacklist');
-var React = require('react/addons');
+var React = require('react');
 var Popup = require('./Popup');
 var DatePicker = require('./DatePicker');
 var classnames = require('classnames');

src/ui/FieldControl.js
@@ -1,6 +1,6 @@
 var blacklist = require('blacklist');
 var classnames = require('classnames');
-var React = require('react/addons');
+var React = require('react');
 
 module.exports = React.createClass({
 	displayName: 'FieldControl',

commit 1bbfd4e4096aafbd6ead964fbb652acccef66b93

Fix flicker issue when leaving with AlertBar
Introduced by https://github.com/touchstonejs/touchstonejs/commit/5d82228#diff-b96a524efaeada2754b4561550de0ecfR32
See https://github.com/touchstonejs/touchstonejs/blob/5d82228/less/components/Alertbar.less#L70

src/ui/Alertbar.js
@@ -29,7 +29,7 @@ module.exports = React.createClass({
 		var animatedBar = this.props.visible ? <div className={className}>{pulseWrap}</div> : null;
 
 		var component = this.props.animated ? (
-			<ReactCSSTransitionGroup transitionName="Alertbar" transitionEnterTimeout={300} transitionLeaveTimeout={300} component="div">
+			<ReactCSSTransitionGroup transitionName="Alertbar" transitionEnterTimeout={240} transitionLeaveTimeout={240} component="div">
 				{animatedBar}
 			</ReactCSSTransitionGroup>
 		) : <div className={className}>{pulseWrap}</div>;